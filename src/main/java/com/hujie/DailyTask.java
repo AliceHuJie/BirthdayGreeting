773628f4513e0820dbfa4c4a908b9404a9f4fa64
hujie <1262741800@qq.com>
2019-07-28 22:24:15 +0800

    change maven to gradle project and bug fixs

--- src/main/java/com/hujie/DailyTask.java
+++ src/main/java/com/hujie/DailyTask.java
@@ -1,38 +1,30 @@
 package com.hujie;
 
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.TimerTask;
 
-public class DailyTask extends TimerTask{
-    private static String filepath = "employee_records.txt";
-
+public class DailyTask extends TimerTask {
     @Override
     public void run() {
-        try {
-            ArrayList<Person> personList = TXTParseUtils.readTXT(filepath);
-            for(Person person:personList){
-                if (isBirthDay(person.getDate_of_birth())){
-                    String subject = "Happy birtyday!";
-                    String content = "Happy birtyday! dear " + person.getFirst_name() + "!";
-                    String email = person.getEmail();
-
-                    SendMailThread d = new SendMailThread(email, subject, content);
-                    d.start();
-                }
+        ArrayList<Person> personList = TXTParseUtils.readTXT();
+        for (Person person : personList) {
+            if (isBirthDay(person.getDate_of_birth())) {
+                String subject = "Happy birtyday!";
+                String content = "Happy birtyday! dear " + person.getFirst_name() + "!";
+                String email = person.getEmail();
+                SendMailThread d = new SendMailThread(email, subject, content);
+                d.start();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 
-    private static boolean isBirthDay(Date personBirth){
+    private static boolean isBirthDay(Date personBirth) {
         Calendar cal = Calendar.getInstance();
         Date date = cal.getTime();
-        SimpleDateFormat simpleDateFormat= new SimpleDateFormat("MM/dd");
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM/dd");
         String todayDateStr = simpleDateFormat.format(date);
         String birthDateStr = simpleDateFormat.format(personBirth);
         return todayDateStr.equals(birthDateStr);
--- src/main/java/com/hujie/TXTParseUtils.java
+++ src/main/java/com/hujie/TXTParseUtils.java
@@ -1,35 +1,49 @@
 package com.hujie;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
 class TXTParseUtils {
-    private static SimpleDateFormat formatter= new SimpleDateFormat("yyyy/MM/dd");
+    private static SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
+    private static final String fileName = "/employee_records.txt";
 
-    static ArrayList<Person> readTXT(final String filepath) throws IOException {
+    static ArrayList<Person> readTXT() {
         ArrayList<Person> personList = new ArrayList<>();
-        BufferedReader file = new BufferedReader(new FileReader(filepath));
-        String line;
-        file.readLine();
-        while ((line = file.readLine()) != null) {
-            if (!line.equals("0"))
-            {
-                if (!line.equals(""))
-                {
-                    String l = new String(line.getBytes());
-                    Person p = parseString2Person(l);
-                    if(p != null){
-                        personList.add(p);
-                    }
+        BufferedReader br = null;
+        InputStream is = null;
+        try {
+            is = TXTParseUtils.class.getResourceAsStream(fileName);
+            br = new BufferedReader(new InputStreamReader(is));
+            String line = "";
+            br.readLine();
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+                Person p = parseString2Person(line);
+                if (p != null) {
+                    personList.add(p);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
         }
-        file.close();
         return personList;
     }
 
@@ -37,11 +51,11 @@ class TXTParseUtils {
         Person person = null;
         String[] fields = line.split(",");
 
-        if(fields.length == 4){
+        if (fields.length == 4) {
             Date date_of_birth;
             try {
                 date_of_birth = formatter.parse(fields[2]);
-                person = new Person(fields[0], fields[1],date_of_birth,fields[3]);
+                person = new Person(fields[0], fields[1], date_of_birth, fields[3]);
             } catch (ParseException e) {
                 e.printStackTrace();
             }
